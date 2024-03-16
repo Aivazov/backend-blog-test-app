690dec5317e3b8afdf3c3a0d213d3e7cd8bd5369
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2024-03-16 14:28:32 +0200

    adding post req and jwt llibrary
    Also allow express to read JSON with express.json() method

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
+app.use(express.json()); //alllow to read JSON before requests
+
 app.get('/', (req, res) => {
   res.send('Hello from express.js in your browser!!');
+});
+
+app.post('/auth/login', (req, res) => {
+  console.log(req.body); //without using express.json() it will return "undefined"
+
+  const token = jwt.sign(); //generating web token
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(2999, (err) => {
